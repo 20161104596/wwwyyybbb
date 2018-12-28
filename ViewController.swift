a746ec6753a44b3e3384ab89016a41ed7ebb7d51
20161104596 <964312086@qq.com>
2018-12-28 17:37:23 +0800

    1
    1

---
+++ ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  wwwyyybbb
+//
+//  Created by 20161104596 on 2018/12/26.
+//  Copyright © 2018 20161104596. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    @IBOutlet weak var wyb1: UIImageView!
+    
+    @IBOutlet weak var wyb2: UIImageView!
+    
+    @IBOutlet weak var wyb3: UIImageView!
+    
+    @IBOutlet weak var mark1: UILabel!
+    
+    @IBOutlet weak var mark2: UILabel!
+    @IBAction func w1(_ sender: Any) {
+    }
+    
+    @IBAction func w2(_ sender: Any) {
+    }
+    
+    @IBAction func w3(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+
+
+}
+
