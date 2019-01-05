0006f7e71d254af4190973ad9d7bfe08d91e889c
20161104596 <964312086@qq.com>
2019-01-05 12:03:25 +0800

    1
    1

--- ViewController.swift
+++ ViewController.swift
@@ -10,21 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var number: UILabel!
+    @IBOutlet weak var number: UITextField!
     
-    @IBOutlet weak var result: UILabel!
     
-    @IBAction func act(_ sender: Any) {
+    @IBOutlet weak var result: UITextField!
+    
+    
+    @IBOutlet weak var ttt: UITextField!
+    
+
+    
+    @IBAction func wyb(_ sender: Any) {
         switch number.text{
-        case "1" :result.text = "\([arc4random()%8+1])"
-        case "2" :result.text = "\([arc4random()%8+1,arc4random()%8+1])"
-        case "3" :result.text = "\([arc4random()%8+1,arc4random()%8+1,arc4random()%8+1])"
-        case "4" :result.text = "\([arc4random()%8+1,arc4random()%8+1,arc4random()%8+1,arc4random()%8+1])"
-        case "5" :result.text = "\([arc4random()%8+1,arc4random()%8+1,arc4random()%8+1,arc4random()%8+1,arc4random()%8+1])"
-        case "6" :result.text = "\([arc4random()%8+1,arc4random()%8+1,arc4random()%8+1,arc4random()%8+1,arc4random()%8+1,arc4random()%8+1])"
+        case "1" :result.text = "\([arc4random()%6+1])"
+        case "2" :result.text = "\([arc4random()%6+1,arc4random()%6+1])"
+        case "3" :result.text = "\([arc4random()%6+1,arc4random()%6+1,arc4random()%6+1])"
+        case "4" :result.text = "\([arc4random()%6+1,arc4random()%6+1,arc4random()%6+1,arc4random()%6+1])"
+        case "5" :result.text = "\([arc4random()%6+1,arc4random()%6+1,arc4random()%6+1,arc4random()%6+1,arc4random()%6+1])"
+        case "6" :result.text = "\([arc4random()%6+1,arc4random()%6+1,arc4random()%6+1,arc4random()%6+1,arc4random()%6+1,arc4random()%6+1])"
         default :result.text = "个数必须是1-6"//如果case情况都不满足
         }
     }
+    
+    @IBAction func w1(_ sender: Any) {
+        ttt.text = " 前进一步 "
+    }
+    
+    @IBAction func w2(_ sender: Any) {
+        ttt.text = " 后退一步 "
+    }
+    
+    @IBAction func w3(_ sender: Any) {
+        ttt.text = " 自罚一杯 "
+    }
+    
+    @IBAction func w4(_ sender: Any) {
+        ttt.text = " 唱一首歌 "
+    }
+    
+    @IBAction func w5(_ sender: Any) {
+        ttt.text = " 学猫叫 "
+    }
+    
+    @IBAction func w6(_ sender: Any) {
+        ttt.text = " 做10个俯卧撑 "
+    }
+    
+    @IBAction func xxxxxxx(_ sender: Any) {
+        number.text = " "
+        result.text = " "
+        ttt.text = " "
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
